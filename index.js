65e1c4519284128acd2fb9a62529c1b6ddf55530
shanecolby <shanecolbyzero8@gmail.com>
2021-07-28 17:15:18 -0500

    add index js and console log button click on view resume

---
+++ index.js
@@ -0,0 +1,9 @@
+// const viewResumeBtn = document.getElementById("viewResume-btn")
+
+// viewResumeBtn.addEventListener("click", function () {
+
+// })
+
+document.getElementById("viewResume-btn").addEventListener("click", function () {
+  console.log("click");
+})
